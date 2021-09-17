dfbb5b38ed20fa0ee065bddf8e5e02453b1f3a47
averak <abe12@mccc.jp>
2021-09-18 05:42:24 +0900

    [feat] create cluster page frame

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 // page components
 import { DashboardComponent } from './components/page/dashboard/dashboard.component';
+import { ClusterComponent } from './components/page/cluster/cluster.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'cluster', component: ClusterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,9 +10,17 @@ import { SharedModule } from './shared/shared.module';
 // components
 import { ProductCardComponent } from './components/presentational/product-card/product-card.component';
 import { DashboardComponent } from './components/page/dashboard/dashboard.component';
+import { ClusterComponent } from './components/page/cluster/cluster.component';
+import { ProductListContentsComponent } from './components/container/product-list-contents/product-list-contents.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductCardComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    ProductCardComponent,
+    DashboardComponent,
+    ClusterComponent,
+    ProductListContentsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
