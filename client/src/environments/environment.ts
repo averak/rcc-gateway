2e70d07e0612b4848695dfaa05574d1e982e995d
averak <abe12@mccc.jp>
2021-09-21 15:00:13 +0900

    [fix] change product description

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -14,14 +14,6 @@ export const environment = {
       display: false,
     },
     {
-      title: 'RDID',
-      description:
-        'RDIDは各プロダクトのシングルサインオンを提供するIDプロバイダです。会員のアカウントを管理し、権限制御を行うことができます。',
-      link: '/rdid',
-      version: '開発中',
-      display: true,
-    },
-    {
       title: 'Cluster',
       description:
         'Clusterは部室の利用を予約するためのサービスです。コロナ禍の部室は入室制限があるので、事前に申請してから入室しましょう。',
@@ -30,9 +22,17 @@ export const environment = {
       display: true,
     },
     {
+      title: 'RDID',
+      description:
+        'RDIDは各プロダクトのシングルサインオンを提供するIDプロバイダです。会員のアカウントを管理し、権限制御を行うことができます。',
+      link: '/rdid',
+      version: '開発中',
+      display: true,
+    },
+    {
       title: 'IAM',
       description:
-        'IAMはロールベースアクセス制御システムです。プロダクトごとに別々の権限を割り当てることができます。',
+        'IAMはロールベースアクセス制御システムです。RDIDと連携して、プロダクトごとに権限を割り当てることができます。',
       link: '/iam',
       version: '開発中',
       display: true,
