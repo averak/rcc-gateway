98584d2a1c707bfd4d42f7eea7fc9ee69dfbdb42
averak <abe12@mccc.jp>
2021-09-22 05:15:08 +0900

    [chore] install angular calendar modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,27 @@ import { AppComponent } from './app.component';
 
 // shared module
 import { SharedModule } from './shared/shared.module';
+
+// angular calendar
+import { FlatpickrModule } from 'angularx-flatpickr';
+import {
+  CalendarModule,
+  DateAdapter,
+  CalendarDateFormatter,
+  CalendarNativeDateFormatter,
+  DateFormatterParams,
+} from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+@Injectable()
+class CustomDateFormatter extends CalendarNativeDateFormatter {
+  public weekViewHour({ date, locale }: DateFormatterParams): string {
+    return new Intl.DateTimeFormat('ca', {
+      hour: 'numeric',
+      minute: 'numeric',
+    }).format(date);
+  }
+}
 
 // dashboard components
 import { DashboardComponent } from './component/dashboard/dashboard.component';
@@ -39,7 +60,24 @@ import { TopicPathComponent } from './component/common/topic-path/topic-path.com
     SidenavComponent,
     TopicPathComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot(
+      {
+        provide: DateAdapter,
+        useFactory: adapterFactory,
+      },
+      {
+        dateFormatter: {
+          provide: CalendarDateFormatter,
+          useClass: CustomDateFormatter,
+        },
+      }
+    ),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
