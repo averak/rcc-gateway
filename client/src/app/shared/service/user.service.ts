58dadb65f235fffa7f2f8055a9721e566f99417e
averak <abe12@mccc.jp>
2021-09-23 15:12:52 +0900

    [feat] get login user

---
+++ client/src/app/shared/service/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpBaseService } from 'src/app/shared/service/http-base.service';
+import { UserModel } from 'src/app/model/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  loginUser: BehaviorSubject<UserModel> = new BehaviorSubject<UserModel>({} as UserModel);
+
+  constructor(private httpBaseService: HttpBaseService) {}
+
+  getLoginUser(): Observable<UserModel> {
+    if (Object.keys(this.loginUser.getValue()).length === 0) {
+      this.fetchLoginUser();
+    }
+
+    return this.loginUser;
+  }
+
+  fetchLoginUser(): void {
+    this.httpBaseService
+      .getRequest<UserModel>(`${environment.API_BASE_URL}/api/users/me`)
+      .subscribe(
+        (user: UserModel) => {
+          this.loginUser.next(user);
+        },
+        (error) => this.loginUser.error(error)
+      );
+  }
+}
