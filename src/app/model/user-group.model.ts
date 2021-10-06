e7cd0567a180a1f8af9723301126499909dc646a
averak <abe12@mccc.jp>
2021-10-06 14:22:50 +0900

    [feat] add user group model

---
+++ src/app/model/user-group.model.ts
@@ -0,0 +1,14 @@
+import { UserModel } from './user.model';
+import { UserRoleEnum } from '../enums/user-role.enum';
+
+export type UserGroupRole = {
+  productName: string;
+  role: UserRoleEnum;
+};
+
+export type UserGroupModel = {
+  name: string;
+  description: string;
+  roles: UserGroupRole[];
+  members: UserModel[];
+};
